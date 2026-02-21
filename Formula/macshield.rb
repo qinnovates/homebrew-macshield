d9e36d514d658c6d9aa4e1a1d67af6b5044df35f
Kevin Qi <qikevinl@users.noreply.github.com>
2026-02-21 10:12:11 -0500

    Update SHA256 for v0.3.0

--- Formula/macshield.rb
+++ Formula/macshield.rb
@@ -2,7 +2,7 @@ class Macshield < Formula
   desc "Network-aware macOS security hardening"
   homepage "https://github.com/qinnovates/macshield"
   url "https://github.com/qinnovates/macshield/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "22a8871bec6f1e4a0f69343f21f4647d34a18799bd7c5e34e331060ea4c1dbec"
+  sha256 "04c63e93d9d10a19f7851282fe3dc20e22be214a586fdf8e9c741659483932f0"
   license "Apache-2.0"
 
   def install
